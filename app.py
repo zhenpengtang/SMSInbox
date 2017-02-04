6539dfb4ef69a7318d51c4836ba868dc67f5e932
zhenpengtang <tangzhenpeng@gmail.com>
2017-02-04 09:07:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,12 +29,12 @@ def get_sms_list_sort(sms_list):
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        if self.get_argument("myid")=="52599d9d8aa170e7ec476a70b1bf1c93":
+        if self.get_argument("myid")=="XXXXXXXXX md5sum code XXXXXXXXXXX":
             sms_list = getSMSList()
             self.render("templates/index.html",smslist=get_sms_list_sort(sms_list))
 
     def post(self):
-        if self.get_argument("myid")=="42062e9f759545e081c2b85fa77d591f":
+        if self.get_argument("myid")=="XXXXXXXXX another md5sum code XXXXXXXXXXX":
             number=self.get_argument("number").encode("utf-8")
             datetime=self.get_argument("datetime").encode("utf-8")
             text=self.get_argument("text").encode("utf-8")
